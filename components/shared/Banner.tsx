63108e6fa1e5caf8f082c110efa7df5acc2650cd
StephenBernard98 <stephenbenard3@gmail.com>
2024-02-11 13:04:54 +0100

    new commit

--- components/shared/Banner.tsx
+++ components/shared/Banner.tsx
@@ -4,10 +4,9 @@ import { mooli } from "@/fonts";
 import Image from "next/image";
 
 const Banner = async () => {
-  // const { sessionClaims } = auth();
-  // const userId = sessionClaims?.userId as string;
-  // const user = await getUserById(userId);
-  // console.log(user)
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+  const user = await getUserById(userId);
 
   return (
     <main className="mt-[5rem]  max-w-[1000px] mx-auto">
@@ -17,23 +16,22 @@ const Banner = async () => {
             <h1 className={` text-6xl mr-3 text-white font-extrabold  mb-4`}>
               Welcome Back,
             </h1>
-            {/* <p className={`text-6xl mr-3 text-white font-extrabold`}>
+            <p className={`text-6xl mr-3 text-white font-extrabold`}>
               {user?.username
                 ? `${user.username
                     .charAt(0)
                     .toUpperCase()}${user.username.slice(1)}!`
                 : "Guest"}
-            </p> */}
+            </p>
           </div>
           <div className="border-[20px] border-white border-r-transparent rounded-full">
-            {/* <Image
+            <Image
               src={user.photo}
               alt="user-img"
               width={250}
               height={250}
               className="rounded-full border-[15px] border-slate-800"
-            /> */}
-            Hello Staff
+            />
           </div>
         </div>
       </section>
